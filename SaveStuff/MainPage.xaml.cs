bbc926c70554e225941ed7a1f7c83863141f0eea
alex110302 <hansenaalexander@gmail.com>
2024-10-11 13:42:03 -0700

    Made small persistent data app

---
+++ SaveStuff/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace SaveStuff;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void btnSave_OnClicked(object sender, EventArgs e)
+    {
+        Preferences.Set("Username", txtUsername.Text); // saves the text of the username text box in a key value pair
+    }
+
+    private void btnRead_OnClicked(object sender, EventArgs e)
+    {
+        txtUsername.Text = Preferences.Get("Username", "");
+    }
+
+    private void btnClear_OnClicked(object sender, EventArgs e)
+    {
+        txtUsername.Text = string.Empty;
+    }
+}
